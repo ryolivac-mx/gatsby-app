af33760e0194b4e5f52b42a72f59fec0e411cfab
Romel Yair Oliva Cruz <ryolivac@pop-os.localdomain>
2020-07-30 11:58:20 -0500

    Se agrega el autor a about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,9 @@ export default ({ data }) => (
     <h2>{data.site.siteMetadata.title}</h2>
     <div>{data.site.siteMetadata.description}</div>
     <Link to="/">Back to home</Link>
+   <br />   
+   <h4>{data.site.siteMetadata.author}</h4>
+    
   </React.Fragment>
 )
 
